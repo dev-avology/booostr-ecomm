0841914692a350326c937c60feb36b29883e66ae
sam110386 <vinodk110386@gmail.com>
2023-09-08 13:39:51 +0530

    updates

--- script/resources/views/components/media/mediamodal.blade.php
+++ script/resources/views/components/media/mediamodal.blade.php
@@ -5,23 +5,19 @@
 <div class="modal-dialog modal-xl">
    <div class="modal-content">
       <div class="modal-header">
-         <!-- <ul class="nav nav-tabs" id="myTab3" role="tablist">
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+         <span aria-hidden="true">&times;</span>
+         </button>
+      </div>
+      <div class="modal-body">
+         <ul class="nav nav-tabs" id="myTab3" role="tablist">
             <li class="nav-item">
                <a class="nav-link active" id="home-tab3" data-toggle="tab" href="#upload_area" role="tab" aria-controls="home" aria-selected="true">Upload File</a>
             </li>
             <li class="nav-item">
                <a class="nav-link" id="profile-tab3" data-toggle="tab" href="#media_list" role="tab" aria-controls="profile" aria-selected="false">Media List</a>
             </li>
-         </ul> -->
-         <div class="nav nav-tabs" id="myTab3" role="tablist">
-            <button class="nav-link active" id="home-tab3" data-bs-toggle="tab" data-bs-target="#upload_area" type="button" role="tab" aria-controls="home" aria-selected="true">Upload File</button>
-            <button class="nav-link" id="profile-tab3" data-bs-toggle="tab" data-bs-target="#media_list" type="button" role="tab" aria-controls="profile" aria-selected="false">Media List</button>
-         </div>
-         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-         <span aria-hidden="true">&times;</span>
-         </button>
-      </div>
-      <div class="modal-body">
+         </ul>
          <div class="tab-content" id="myTabContent2">
             <div class="tab-pane fade show active" id="upload_area" role="tabpanel" aria-labelledby="home-tab3">
                <form method="post" action="{{ route('seller.media.store') }}" class="dropzone dropzones">
@@ -62,19 +58,6 @@
                   </div>
                </div>
             </div>
-
-            <nav>
-               <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                  <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
-                  <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
-                  <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
-               </div>
-               </nav>
-               <div class="tab-content" id="nav-tabContent">
-                  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home...</div>
-                  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">profile...</div>
-                  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">contact...</div>
-               </div>
          </div>
          <div class="modal-footer">
             <button type="button" class="btn btn-danger media-single-dismiss" data-dismiss="modal">{{ __('Close') }}</button>
