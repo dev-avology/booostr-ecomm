a48b48191fd8e5f20a26e3dac7bc8f4188e43398
sam110386 <vinodk110386@gmail.com>
2023-09-12 11:43:48 +0530

    checkout feature updates

--- script/resources/views/layouts/checkout.blade.php
+++ script/resources/views/layouts/checkout.blade.php
@@ -15,19 +15,16 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="shortcut icon" type="image/x-icon" href="{{ asset('uploads/'.tenant('uid').'/favicon.ico') }}">
       <!-- Web Font -->
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="{{ asset('theme/resto/css/bootstrap.min.css') }}">
       <link rel="stylesheet" href="{{ asset('theme/resto/css/icofont.css') }}">
       <link rel="stylesheet" href="{{ asset('theme/resto/css/nice-select.css') }}">
-      <link rel="stylesheet" href="{{ asset('theme/resto/css/animate.css') }}">
-      <link rel="stylesheet" href="{{ asset('theme/resto/css/tiny-slider.css') }}">
-      <link rel="stylesheet" href="{{ asset('theme/resto/css/glightbox.min.css') }}">
       <link rel="stylesheet" href="{{ asset('theme/resto/css/perfect-scrollbar.css') }}">
       <!-- Theme Styles -->
-      <link rel="stylesheet" href="{{ asset('theme/resto/css/reset.css') }}">
-      <link rel="stylesheet" href="{{ asset('theme/resto/style.css') }}">
-      <link rel="stylesheet" href="{{ asset('theme/resto/css/responsive.css') }}">
-      <link rel="stylesheet" href="{{ asset('theme/helper.css') }}">
+      <link rel="stylesheet" href="{{ asset('checkout/css/style.css') }}">
       @stack('css')
       {{ load_header() }}
    </head>
@@ -85,12 +82,6 @@
 <script src="{{ asset('theme/resto/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('theme/resto/js/nice-select.js') }}"></script>
 <script src="{{ asset('theme/resto/js/wow.min.js') }}"></script>
-<script src="{{ asset('theme/resto/js/tiny-slider.js') }}"></script>
-<script src="{{ asset('theme/resto/js/glightbox.min.js') }}"></script>
-<script src="{{ asset('theme/resto/js/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('theme/resto/js/main.js') }}"></script>
-<script src="{{ asset('theme/helper.js?v=1.0') }}"></script>
-<script src="{{ asset('theme/resto/js/theme-helper.js') }}"></script>
 
 @stack('js')
 {{ load_footer() }}
