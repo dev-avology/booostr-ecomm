62552f5f6bfdb5ee40a3d2b902b655c85e9eca68
kn-iqsay <kn@int.iqsay.com>
2023-09-21 16:24:28 +0530

    fix

--- script/app/Http/Controllers/Store/CheckoutController.php
+++ script/app/Http/Controllers/Store/CheckoutController.php
@@ -403,7 +403,11 @@ class CheckoutController extends Controller
             }
 
             DB::commit();
-            Cart::instance('default')->destroy();
+            if(Session::has('cartid')){
+                $cartid=Session::get('cartid');
+                Cart::instance('default')->destroy($cartid);
+            }
+
             return redirect()->away($redirect_url . '/?type=success&message=Thanks for your purchase. Your order number is ' . $order->invoice_no);
         } catch (\Throwable $th) {
             DB::rollback();
