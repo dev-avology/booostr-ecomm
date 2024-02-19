26be497f2e48320ad2a5e578693b5b21ec193e70
Avology <dharmendraavology@gmail.com>
2024-02-19 19:00:57 +0530

    aaa

--- script/resources/views/mail/posusermail.blade.php
+++ script/resources/views/mail/posusermail.blade.php
@@ -111,7 +111,7 @@
                         class="spac-top spac-btm">
                         <span
                             style="font-weight: bold; font-family: 'Nunito', 'Segoe UI', Arial; color: #3c3c3c; margin: 0; padding-left: 20px;">
-                            Order #: <span style="font-weight: 500;">{{$data['orderid']}}</span>
+                            Order #: <span style="font-weight: 500;">{{$data['orderid'] ?? "NA"}}</span>
                         </span>
                         <br>
                       
