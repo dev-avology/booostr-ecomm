de36998df8d24bdd2d37cdef055b563a47d52551
Avology <dharmendraavology@gmail.com>
2024-05-27 11:11:20 +0530

    added

--- script/app/Http/Controllers/Store/CheckoutController.php
+++ script/app/Http/Controllers/Store/CheckoutController.php
@@ -76,7 +76,8 @@ class CheckoutController extends Controller
 
     public function redirect_to_checkout(Request $request,$cartid,$redirect_url='/')
     {
-        dd($request->formData);
+        print_r($request->formData);
+        dd('dd');
         if (empty($cartid)) {
             return redirect()->to($redirect_url)->with(['type' => 'error','message' => 'Oops something went wrong']);
         }
