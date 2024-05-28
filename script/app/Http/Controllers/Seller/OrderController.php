3fb4ed80075ef435b1d2e95a770e403ed6b0b096
Nishant Dubey <nishant.avology@gmail.com>
2024-05-28 17:49:57 +0530

    fix

--- script/app/Http/Controllers/Seller/OrderController.php
+++ script/app/Http/Controllers/Seller/OrderController.php
@@ -269,7 +269,6 @@ class OrderController extends Controller
            DB::commit();
         } catch (\Throwable $th) {
             DB::rollback();
-            dd($th);
             $errors['errors']['error']='Oops something wrong';
             return response()->json($errors,401);
         } 
