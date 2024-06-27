a440362c88e739d6431d745844e62ad9dbcf234e
Nishant Dubey <nishant.avology@gmail.com>
2024-06-27 13:05:37 +0530

    fix

--- script/app/Http/Controllers/WelcomeController.php
+++ script/app/Http/Controllers/WelcomeController.php
@@ -72,8 +72,7 @@ class WelcomeController extends Controller
             $ordermeta=json_decode($order->ordermeta->value ?? '');
             $club_info = tenant_club_info();
             
-            if(!empty($ordermeta)){                
-            $name = !empty($ordermeta->name) ? explode(' ',$ordermeta->name) : '' ;
+
             if($order->order_from == 4 || $order->order_from == 5){
                 $customer_tag = 'POS store customer';
                 $country = $ordermeta->billing->country??'USA';
@@ -81,6 +80,7 @@ class WelcomeController extends Controller
                 $city = $ordermeta->billing->city??'Alameda';
                 $state = $ordermeta->billing->state??'California';
                 $post_code = $ordermeta->billing->post_code??'94501';
+                $donor_name = isset($ordermeta['name'])? $ordermeta['name'].' (POS Order)':'Guest User'.' (POS Order)';
             }else{
                 $customer_tag = 'online store customer';
 
@@ -89,7 +89,13 @@ class WelcomeController extends Controller
                 $city = $ordermeta->billing->city??'';
                 $state = $ordermeta->billing->state??'';
                 $post_code = $ordermeta->billing->post_code??'';
+                $donor_name = isset($ordermeta['name'])? $ordermeta['name'].' (Online Order)':'Guest User'.' (Online Order)';
+
             }
+
+            if(!empty($ordermeta)){                
+            $name = !empty($ordermeta->name) ? explode(' ',$ordermeta->name) : '' ;
+           
 
             $contact_manager_data = array(
                 'first_name' => $name[0]??'',
@@ -109,7 +115,6 @@ class WelcomeController extends Controller
                 'customer_tag' => $customer_tag,
             );	 
             $user_recipt = [
-                'contact_mgr_data'=>$contact_manager_data,
                 'receipts_date'=>Carbon::now()->setTimezone(config('app.timezone')),
                 'receipt_title'=>$ordermeta->name,
                 'receipent_org'=>$club_info['club_name'].' Store',
@@ -120,6 +125,67 @@ class WelcomeController extends Controller
                 'camp_id'=>$order->invoice_no,
                 'order_total'=>$order->total,
             ];
+
+
+
+            $order_date = Carbon::parse($order->created_at)->format('Y-m-d');
+            $qty = $order->orderitems[0]['qty'];
+            $product_amount = $order->orderitems[0]['amount'];
+            $sub_total = $product_amount*$qty;
+            $sales_tax = $order->tax;
+            $order_total = $order->total;
+        
+            if(isset($order->ordermeta)){
+                $ordermeta=json_decode($order->ordermeta->value ?? '',true);
+            }
+    
+            $gateway=Getway::find($order->getway_id);
+    
+    
+            //$jsonString = $order->shippingwithinfo['info'];
+    
+            $jsonString = $order->shippingwithinfo['info'];
+            // Decode the JSON string into a PHP array
+            $shipping_data = json_decode($jsonString, true);
+    
+            $credit_card_fee = $shipping_data['credit_card_fee'];
+            $booster_platform_fee = $shipping_data['booster_platform_fee'];
+            $processing_fees = $credit_card_fee+$booster_platform_fee;
+    
+            $net_recieved_amount = $order_total-($sales_tax+$processing_fees);
+    
+            $shipped_and_fullfilldate = Carbon::parse($order->updated_at)->format('Y-m-d');
+
+            $postData = json_encode(['contact_mgr_data'=>$contact_manager_data,
+            'user_recipt'=>$user_recipt,
+            'category_type'=> 'Booostr Ecommerce',
+            'booster_id' =>Tenant('club_id'),
+            'coaid'=>41,
+            'contactname'=>$ordermeta['name'],
+            //'memo'=>'Booostr Ecommerce',
+            'user_id' =>  $ordermeta['wpuid']??0,
+            'revenue_name'=>'4-850 Booostr Ecommerce',
+            'transaction_type'=>'I',
+            'sales_tax_collected' => $sales_tax > 0 ? 'Yes':'No',
+            'net_revenue'=>$net_recieved_amount,
+            'transaction_amount'=>$order_total,
+            'expense_category'=>'Revenue',
+            'receipts_issued'=> 'Yes',
+            'status'=>1,
+            'donor_name'=>$ordermeta['name'].' (Online Order)',
+            'created'=>$order->placed_at,
+            'modified'=>Carbon::now()->setTimezone(config('app.timezone')),
+            'payement_method'=>($gateway->name == 'cash') ? 0 : 3,
+            'invoicenumber'=>$order->invoice_no,
+            'invoicreatedate'=>$order->placed_at,
+            'invoiceprocessingfee'=>$processing_fees,
+            'invoicesalestax'=> $sales_tax,
+            'invoiceopt'=>$order->invoice_no,
+            'deposite_date'=>$order->captured_at,
+            'transfer_refund_date'=> ($post_type == 'refund') ? $order->refunded_at : null,
+            'record_type' => $post_type,
+          ]);
+
 
 
             $url = env("WP_API_URL");
@@ -133,7 +199,7 @@ class WelcomeController extends Controller
              curl_setopt($ch, CURLOPT_USERAGENT, 'Tantent store');   
              curl_setopt($ch, CURLOPT_URL, $url);
              curl_setopt($ch, CURLOPT_POST, 1);
-             curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($user_recipt)); // Encode data as URL-encoded 
+             curl_setopt($ch, CURLOPT_POSTFIELDS, $postData); // Encode data as URL-encoded 
              curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); // Set content type header
              curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
          
